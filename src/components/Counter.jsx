21f7881130dd2b54ffa95c6542e2c6af3a7f7b4b
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-04-17 15:20:40 +0300

    Solve Exercise 1

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount((prevCounter) => prevCounter + 1);
+  };
+  useEffect(() => {
+    document.addEventListener('mousedown', incrementCount);
+    return () => {
+      document.removeEventListener('mousedown', incrementCount);
+    };
+  });
+  return <p>{count}</p>;
+}
